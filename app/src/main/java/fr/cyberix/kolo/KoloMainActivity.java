6e33931a39709061a896f33872a86db94f740bd3
HaroldMT <tm.harold@gmail.com>
2018-06-01 05:48:11 +0200

    Thierry - I added runtime Permissions Access for the App

--- app/src/main/java/fr/cyberix/kolo/KoloMainActivity.java
+++ app/src/main/java/fr/cyberix/kolo/KoloMainActivity.java
@@ -1,8 +1,17 @@
 package fr.cyberix.kolo;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +21,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class KoloMainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Context mContext;
+    private Activity mActivity;
+
+    private static final int MY_PERMISSIONS_REQUEST_CODE = 123;
+
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +40,9 @@ public class KoloMainActivity extends AppCompatActivity
         setContentView(R.layout.activity_kolo_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mContext = getApplicationContext();
+        mActivity = KoloMainActivity.this;
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -40,7 +61,117 @@ public class KoloMainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+
+
+
+
+
+        if(ContextCompat.checkSelfPermission(mActivity,Manifest.permission.SEND_SMS)
+                + ContextCompat.checkSelfPermission(
+                mActivity,Manifest.permission.READ_SMS)
+                + ContextCompat.checkSelfPermission(
+                mActivity,Manifest.permission.RECEIVE_SMS)
+                + ContextCompat.checkSelfPermission(
+                mActivity,Manifest.permission.READ_CONTACTS)
+                + ContextCompat.checkSelfPermission(
+                mActivity,Manifest.permission.INTERNET)
+                != PackageManager.PERMISSION_GRANTED){
+
+            // Do something, when permissions not granted
+            if(ActivityCompat.shouldShowRequestPermissionRationale(
+                    mActivity,Manifest.permission.SEND_SMS)
+                    || ActivityCompat.shouldShowRequestPermissionRationale(
+                    mActivity,Manifest.permission.RECEIVE_SMS)
+                    || ActivityCompat.shouldShowRequestPermissionRationale(
+                    mActivity,Manifest.permission.READ_SMS)
+                    || ActivityCompat.shouldShowRequestPermissionRationale(
+                    mActivity,Manifest.permission.READ_CONTACTS)
+                    || ActivityCompat.shouldShowRequestPermissionRationale(
+                    mActivity,Manifest.permission.INTERNET)){
+                // If we should give explanation of requested permissions
+
+                // Show an alert dialog here with request explanation
+                AlertDialog.Builder builder = new AlertDialog.Builder(mActivity);
+                builder.setMessage("Contacts permissions" +
+                        " SMS Permissions are required to access kolo App.");
+                builder.setTitle("Please grant those permissions");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        ActivityCompat.requestPermissions(
+                                mActivity,
+                                new String[]{
+                                        Manifest.permission.SEND_SMS,
+                                        Manifest.permission.RECEIVE_SMS,
+                                        Manifest.permission.READ_SMS,
+                                        Manifest.permission.READ_CONTACTS,
+                                        Manifest.permission.INTERNET,
+                                },
+                                MY_PERMISSIONS_REQUEST_CODE
+                        );
+                    }
+                });
+                builder.setNeutralButton("Cancel",null);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }else{
+                // Directly request for required permissions, without explanation
+                ActivityCompat.requestPermissions(
+                        mActivity,
+                        new String[]{
+                                Manifest.permission.SEND_SMS,
+                                Manifest.permission.RECEIVE_SMS,
+                                Manifest.permission.READ_SMS,
+                                Manifest.permission.READ_CONTACTS,
+                                Manifest.permission.INTERNET,
+                        },
+                        MY_PERMISSIONS_REQUEST_CODE
+                );
+            }
+        }else {
+            // Do something, when permissions are already granted
+            Toast.makeText(mContext,"Permissions already granted",Toast.LENGTH_SHORT).show();
+        }
     }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        switch (requestCode){
+            case MY_PERMISSIONS_REQUEST_CODE:{
+                // When request is cancelled, the results array are empty
+                if(
+                        (grantResults.length >0) &&
+                                (grantResults[0]
+                                        + grantResults[1]
+                                        + grantResults[2]
+                                        + grantResults[3]
+                                        == PackageManager.PERMISSION_GRANTED
+                                )
+                        ){
+                    // Permissions are granted
+                    Toast.makeText(mContext,"Permissions granted.",Toast.LENGTH_SHORT).show();
+                }else {
+                    closeNow();
+                    // Permissions are denied
+                    Toast.makeText(mContext,"Permissions denied.",Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
+
+    private void closeNow() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            finishAffinity();
+        } else {
+            finish();
+        }
+    }
+
 
     @Override
     public void onBackPressed() {
