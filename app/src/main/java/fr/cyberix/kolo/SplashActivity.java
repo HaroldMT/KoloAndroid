a38100c4105f003fdc3c0a7eb820653e27b63ab7
HaroldMT <tm.harold@gmail.com>
2018-06-03 22:11:57 +0100

    H - Config Initialization

--- app/src/main/java/fr/cyberix/kolo/SplashActivity.java
+++ app/src/main/java/fr/cyberix/kolo/SplashActivity.java
@@ -6,6 +6,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Calendar;
+import java.util.Date;
+
+import fr.cyberix.kolo.helpers.ConfigHelper;
+import fr.cyberix.kolo.helpers.DateHelper;
+import fr.cyberix.kolo.helpers.KoloConstants;
+import fr.cyberix.kolo.helpers.KoloHelper;
+
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
@@ -173,7 +181,25 @@ public class SplashActivity extends AppCompatActivity {
 //        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.splashtransition);
 //        tv.startAnimation(myanim);
 //        iv.startAnimation(myanim);
-        startActivity(new Intent(SplashActivity.this, SignUpActivity.class));
+
+        KoloHelper.initialize(this, this, this);
+
+        if (ConfigHelper.getRegistering())
+            startActivity(new Intent(getApplicationContext(), SignUpConfirmationActivity.class));
+        else if (ConfigHelper.getRegistered()) {
+            Boolean gotoLoginActivity = true;
+            if (ConfigHelper.getAccountInfo().getAuthenticated()) {
+                Date date = ConfigHelper.getAccountInfo().getLastAuthenticationTime();
+                DateHelper dateHelper = DateHelper.DateDiff(date, Calendar.getInstance().getTime());
+                if (dateHelper.diffInMin < KoloConstants.forceLoginAfterShutdownTimeLimit) {
+                    gotoLoginActivity = false;
+                    startActivity(new Intent(getApplicationContext(), DashboardActivity.class));
+                }
+            }
+            if (gotoLoginActivity)
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+        } else startActivity(new Intent(getApplicationContext(), SignUpActivity.class));
+
 //        try {
 //            Thread.sleep(1500);
 //        } catch (InterruptedException e) {
