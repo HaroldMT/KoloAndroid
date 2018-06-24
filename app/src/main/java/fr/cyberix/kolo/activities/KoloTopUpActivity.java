bf8fc333e4bddfc8de120095cfe5e9a0e125f0fc
WilfriedTIANI <wilfriedtiani@gmail.com>
2018-06-24 19:56:56 +0100

    - KoloTopUpActivity.java : Updated - string.xml : Updated to add TopUp provider list - activiy_kolo_eneo_bill_payment.xml : Updated to remove error on launch - activiy_kolo_top_up.xml : Updated to add TopUp provider on Spinner

--- app/src/main/java/fr/cyberix/kolo/activities/KoloTopUpActivity.java
+++ app/src/main/java/fr/cyberix/kolo/activities/KoloTopUpActivity.java
@@ -2,14 +2,31 @@ package fr.cyberix.kolo.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import fr.cyberix.kolo.R;
+import fr.cyberix.kolo.helpers.KoloHelper;
 
 public class KoloTopUpActivity extends AppCompatActivity {
-	
+	String TAG;
+	@BindView(R.id.airtime_amount_text)
+	EditText airtimeAmount;
+	@BindView(R.id.airtime_buy_btn)
+	Button buyAirtime;
+	@BindView(R.id.airtime_provider_spinner)
+    Spinner airtimeProvider;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_kolo_top_up);
+		TAG = this.getLocalClassName();
+		ButterKnife.setDebug(true);
+		ButterKnife.bind(this);
+		KoloHelper.setActivity(this);
 	}
 }
